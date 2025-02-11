a8c1354d265cddf03aaa5fac14aec0959911d6a0
hein <heinrichcapatina958@gmail.com>
2025-02-11 20:10:42 +0800

    second commit

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -107,7 +107,6 @@ int main(void)
         HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
       }
       HAL_Delay(20);
-      HAL_Delay(5);
     }
     // HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
     /* USER CODE END WHILE */
